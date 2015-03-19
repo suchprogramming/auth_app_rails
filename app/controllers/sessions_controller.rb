9c05ba0f033b332b549323b9f684c1fdcf56c035
Tyler <whitsett.tyler@gmail.com>
2015-03-18 17:11:35 -0700

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #try to authenticate the user - if they authenticate successfully, an instance of the User model is returned
+    @user = User.authenticate(params[:email], params[:password])
+    #if an instance is returned and @user is not nil...
+    if @user
+      #let the user know they've been logged in with a flash message
+      flash[:success] = "You've been logged in."
+      #THIS IS THE MOST IMPORTANT PART. Actually log the user in by storing their ID in the session hash with the [:user_id] key!
+      session[:user_id] = @user.id
+      #then redirect them to the homepage
+      redirect_to "/"
+    else
+      #whoops, either the user wasn't in the database or their password is incorrect, so let them know, then redirect them back to the log in page
+      flash[:danger] = "There was a problem logging you in."
+      redirect_to log_in_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You've been logged out successfully."
+    redirect_to "/"
+  end
+
+end
